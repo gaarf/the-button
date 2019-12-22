c7efed95a45ec20a3888c67a948cba0c4cc2979f
Adrien Cahen <onlineallthetime@gmail.com>
2019-12-22 12:47:05 -0800

    Rinkeby check

--- src/containers/TheButton/getWeb3.tsx
+++ src/containers/TheButton/getWeb3.tsx
@@ -2,19 +2,23 @@ import Web3 from "web3";
 
 export function getWeb3(): Promise<Web3> {
   // @ts-ignore
-  const { ethereum, web3 } = window;
+  const { ethereum, location, web3 } = window;
 
-  const reload = () => window.location.reload(true);
+  const reload = () => location.reload(true);
 
   return new Promise(async (resolve, reject) => {
     if (ethereum) {
       ethereum.autoRefreshOnNetworkChange = false;
+      await ethereum.enable();
       ethereum.on("networkChanged", reload);
       ethereum.on("accountsChanged", reload);
-      await ethereum.enable();
+
+      if (ethereum.chainId !== "0x4") {
+        reject(new Error("Please select the Rinkeby Test Network!"));
+        return;
+      }
       resolve(new Web3(ethereum));
-    }
-    else if (web3) {
+    } else if (web3) {
       // Acccounts always exposed
       resolve(new Web3(web3.currentProvider));
     }
