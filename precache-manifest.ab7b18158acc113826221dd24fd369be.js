680e7eafb31a20ae56fdf07b80bf75eef0d50673
Adrien Cahen <onlineallthetime@gmail.com>
2019-12-22 12:34:15 -0800

    Updates

---
+++ precache-manifest.ab7b18158acc113826221dd24fd369be.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2ea4b2c10d9f2cae4835d03ff850be",
+    "url": "/the-button/index.html"
+  },
+  {
+    "revision": "bba499d58a8d9b61a642",
+    "url": "/the-button/static/css/2.95dc13dc.chunk.css"
+  },
+  {
+    "revision": "bba499d58a8d9b61a642",
+    "url": "/the-button/static/js/2.d23dbf64.chunk.js"
+  },
+  {
+    "revision": "5696c26de8041b8d357c97521954a5f0",
+    "url": "/the-button/static/js/2.d23dbf64.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ded8b615dbae06b66287",
+    "url": "/the-button/static/js/main.b4ca3235.chunk.js"
+  },
+  {
+    "revision": "d497adcf6d5386f27df1",
+    "url": "/the-button/static/js/runtime-main.7e0f8039.js"
+  },
+  {
+    "revision": "782154b83b6b419269980b375144b1f7",
+    "url": "/the-button/static/media/icons-16.782154b8.ttf"
+  },
+  {
+    "revision": "7fb844a70d4ff3aa49441242b4149777",
+    "url": "/the-button/static/media/icons-16.7fb844a7.woff"
+  },
+  {
+    "revision": "d4f6722b1ac527dd937155d419ec8ddd",
+    "url": "/the-button/static/media/icons-16.d4f6722b.eot"
+  },
+  {
+    "revision": "90afdd237a469c44c9a1a3b0ff2db197",
+    "url": "/the-button/static/media/icons-20.90afdd23.eot"
+  },
+  {
+    "revision": "c0e4874e23b8c9100e566ca2ca287128",
+    "url": "/the-button/static/media/icons-20.c0e4874e.woff"
+  },
+  {
+    "revision": "d1f0f6c1a1c239a03914f7bd06ccfe68",
+    "url": "/the-button/static/media/icons-20.d1f0f6c1.ttf"
+  }
+]);
--- src/plumbing/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./style";
-export * from "./toaster";
-
-
-export function plural(count: number, singular: string): string {
-  return `${count} ${singular}${count !== 1 ? "s" : ""}`;
-}
